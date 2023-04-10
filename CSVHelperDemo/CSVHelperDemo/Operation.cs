34d8d3fa34f56b3df39bdcf741856c9270fc667e
anandsaikumar <anandsai298@gmail.com>
2023-04-10 09:54:18 +0530

    [sai] Add : by reading csv file export to json file

--- CSVHelperDemo/CSVHelperDemo/Operation.cs
+++ CSVHelperDemo/CSVHelperDemo/Operation.cs
@@ -4,8 +4,9 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
-
+using Newtonsoft.Json;
 
 namespace CSVHelperDemo
 {
@@ -31,6 +32,24 @@ namespace CSVHelperDemo
             {
                 csvwriter.WriteRecords(records);
             }
+        }
+        public static void ImplementCSVToJson()
+        {
+            List<Model> records = new List<Model>();
+            string importFilePath = @"F:\CSVHelperDemo\CSVHelperDemo\CSVHelperDemo\csvFile.csv";
+            string exportFilePath = @"F:\CSVHelperDemo\CSVHelperDemo\CSVHelperDemo\csvFile2.json";
+            using (var reader = new StreamReader(importFilePath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                records = csv.GetRecords<Model>().ToList();
+                Console.WriteLine("reading csv file");
+                foreach (var data in records)
+                {
+                    Console.WriteLine(data.Name + "\n" + data.Email + "\n" + data.PhoneNumber + "\n" + data.Country);
+                }
+            }
+            var json = JsonConvert.SerializeObject(records);
+            File.WriteAllText(exportFilePath, json);
         }
     }
 }
--- CSVHelperDemo/CSVHelperDemo/Program.cs
+++ CSVHelperDemo/CSVHelperDemo/Program.cs
@@ -6,6 +6,7 @@ class program
     static void Main(string[] args)
     {
         Operation.ImplementCSVHandling();
+        Operation.ImplementCSVToJson();
     }
 }
 
